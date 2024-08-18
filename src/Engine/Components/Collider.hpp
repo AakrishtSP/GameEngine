5a13ddb0cacca8f36ea93717cf1421cc18832004
AakrishtSP <akrisht62@gmail.com>
2024-08-18 12:12:51 +0545

    Refactor Collider class to include supportFunction for Rectangle

--- src/Engine/Components/Collider.hpp
+++ src/Engine/Components/Collider.hpp
@@ -16,10 +16,9 @@ public:
     void update(float deltatime) override;
     void renderUpdate(float renderDeltaTime) override{};
     void physicsUpdate(float fixedDeltaTime) override;
+    void renderUpdate(float renderDeltaTime) override;
     Vector2 supportFunction(Circle &circle, Vector2 &direction);
     Vector2 supportFunction(const Rectangle& rect, float rotation, const Vector2& direction);
-    //Vector2 support(Rectangle &rectangle, Vector2 &direction);
-    //Vector2 support(Polygon &polygon, Vector2 &direction);
 
 private:
     mutable std::shared_ptr<Transform2D> transform = nullptr;
