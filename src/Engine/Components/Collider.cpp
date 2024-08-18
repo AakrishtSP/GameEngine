482b910c264bb60538304971cc49bb6d906968b5
AakrishtSP <akrisht62@gmail.com>
2024-08-18 12:17:31 +0545

    Refactor Collider class to include supportFunction for Rectangle

--- src/Engine/Components/Collider.cpp
+++ src/Engine/Components/Collider.cpp
@@ -24,9 +24,7 @@ Vector2 Collider::supportFunction(Circle &circle, Vector2 &direction)
     return circle.center + dir * circle.radius;
 }
 
-Vector2 Collider::supportFunction(const Rectangle &rect, float rotation, const Vector2 &direction) { return Vector2(); }
-
-Vector2 supportFunction(const Rectangle& rect, float rotation, const Vector2& direction) {
+Vector2 Collider::supportFunction(const Rectangle& rect, float rotation, const Vector2& direction) {
 
     Vector2 dir = Normalize(direction);
     Vector2 vertices[4]= {
