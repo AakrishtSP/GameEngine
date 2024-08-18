90f4bbf09b2bdad960533277b4bdf7742828080b
AakrishtSP <akrisht62@gmail.com>
2024-08-18 12:51:58 +0545

    refactor: Update Collider class to separate physics update from general update

--- src/Engine/Components/Collider.hpp
+++ src/Engine/Components/Collider.hpp
@@ -14,6 +14,8 @@ public:
     ~Collider();
 
     void update(float deltatime) override;
+    void renderUpdate(float renderDeltaTime) override{};
+    void physicsUpdate(float fixedDeltaTime) override;
     Vector2 supportFunction(Circle &circle, Vector2 &direction);
     Vector2 supportFunction(const Rectangle& rect, float rotation, const Vector2& direction);
     //Vector2 support(Rectangle &rectangle, Vector2 &direction);
