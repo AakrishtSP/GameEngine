c1a14ea71e1d43e03d75be773b0306f36d891820
Ishan Gautam <isangautam@gmail.com>
2024-08-18 12:47:32 +0545

    collider

--- src/Engine/Components/Collider.cpp
+++ src/Engine/Components/Collider.cpp
@@ -8,33 +8,139 @@ Collider::~Collider()
 {
 }
 
-void Collider::update(float deltaTime)
-{
-}
-void Collider::physicsUpdate(float fixedDeltaTime)
-{
-}
-void Collider::renderUpdate(float renderDeltaTime)
+void Collider::update()
 {
 }
 
-Vector2 Collider::support(Circle &circle, Vector2 &direction)
+Vector2 Collider::supportFunction(Circle &circle, Vector2 &direction)
 {
     Vector2 dir = Normalize(direction);
     return circle.center + dir * circle.radius;
 }
 
+Vector2 Collider::supportFunction(const Rectangle& rect, float rotation, const Vector2& direction) {
+
+    Vector2 dir = Normalize(direction);
+    Vector2 vertices[4]= {
+        {rect.x, rect.y},
+        {rect.x + rect.width, rect.y},
+        {rect.x + rect.width, rect.y + rect.height},
+        {rect.x, rect.y + rect.height}
+    };
+    float cosA = cos(rotation);
+    float sinA = sin(rotation);
+    
+    // Rotate the vertices
+    Vector2 rotationPoint = {rect.x + rect.width / 2, rect.y + rect.height / 2};
+    for (int i = 0; i < 4; i++) {
+        //RotatePoint(vertices[i], rotationPoint, rotation)
+        float x = vertices[i].x - rotationPoint.x;
+        float y = vertices[i].y - rotationPoint.y;
+        vertices[i].x = x * cosA - y * sinA + rotationPoint.x;
+        vertices[i].y = x * sinA + y * cosA + rotationPoint.y;  
+    }
+
+    float maxDot = DotProduct(vertices[0], dir);
+    Vector2 returnVertex = vertices[0];
+    for (int i = 1; i < 4; i++) {
+        float dot = DotProduct(vertices[i], dir);
+        if (dot > maxDot) {
+            maxDot = dot;
+            returnVertex = vertices[i];
+        }
+    }
+
+    return returnVertex;
+}
+
+
+
+/*
 Vector2 Collider::support(Rectangle &rectangle, Vector2 &direction)
 {
     Vector2 dir = Normalize(direction);
 
     return Vector2();
 }
+*/
 
-Vector2 Collider::support(Polygon &polygon, Vector2 &direction)
+/*Vector2 Collider::support(Polygon &polygon, Vector2 &direction)
 {
     Vector2 dir = Normalize(direction);
 
     return Vector2();
 }
+*/
+#include "Collider.hpp"
 
+Collider::Collider()
+{
+}
+
+Collider::~Collider()
+{
+}
+
+void Collider::update()
+{
+}
+
+Vector2 Collider::supportFunction(Circle &circle, Vector2 &direction)
+{
+    Vector2 dir = Normalize(direction);
+    return circle.center + dir * circle.radius;
+}
+
+Vector2 Collider::supportFunction(const Rectangle& rect, float rotation, const Vector2& direction) {
+
+    Vector2 dir = Normalize(direction);
+    Vector2 vertices[4]= {
+        {rect.x, rect.y},
+        {rect.x + rect.width, rect.y},
+        {rect.x + rect.width, rect.y + rect.height},
+        {rect.x, rect.y + rect.height}
+    };
+    float cosA = cos(rotation);
+    float sinA = sin(rotation);
+    
+    // Rotate the vertices
+    Vector2 rotationPoint = {rect.x + rect.width / 2, rect.y + rect.height / 2};
+    for (int i = 0; i < 4; i++) {
+        //RotatePoint(vertices[i], rotationPoint, rotation)
+        float x = vertices[i].x - rotationPoint.x;
+        float y = vertices[i].y - rotationPoint.y;
+        vertices[i].x = x * cosA - y * sinA + rotationPoint.x;
+        vertices[i].y = x * sinA + y * cosA + rotationPoint.y;  
+    }
+
+    float maxDot = DotProduct(vertices[0], dir);
+    Vector2 returnVertex = vertices[0];
+    for (int i = 1; i < 4; i++) {
+        float dot = DotProduct(vertices[i], dir);
+        if (dot > maxDot) {
+            maxDot = dot;
+            returnVertex = vertices[i];
+        }
+    }
+
+    return returnVertex;
+}
+
+
+
+/*
+Vector2 Collider::support(Rectangle &rectangle, Vector2 &direction)
+{
+    Vector2 dir = Normalize(direction);
+
+    return Vector2();
+}
+*/
+
+/*Vector2 Collider::support(Polygon &polygon, Vector2 &direction)
+{
+    Vector2 dir = Normalize(direction);
+
+    return Vector2();
+}
+*/
--- src/Engine/Components/Collider.hpp
+++ src/Engine/Components/Collider.hpp
@@ -13,15 +13,15 @@ public:
     Collider();
     ~Collider();
 
-    void update(float deltaTime) override;
-    void physicsUpdate(float fixedDeltaTime) override;
-    void renderUpdate(float renderDeltaTime) override;
-    Vector2 support(Circle &circle, Vector2 &direction);
-    Vector2 support(Rectangle &rectangle, Vector2 &direction);
-    Vector2 support(Polygon &polygon, Vector2 &direction);
+    void update(float deltatime) override;
+    Vector2 supportFunction(Circle &circle, Vector2 &direction);
+    Vector2 supportFunction(const Rectangle& rect, float rotation, const Vector2& direction);
+    //Vector2 support(Rectangle &rectangle, Vector2 &direction);
+    //Vector2 support(Polygon &polygon, Vector2 &direction);
 
 private:
     mutable std::shared_ptr<Transform2D> transform = nullptr;
     Rectangle rectangle;
     Vector2 offset;
+
 };
