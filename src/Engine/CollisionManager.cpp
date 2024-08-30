e57aed30e955e6c7a7ca1ec2603e9501d224cc26
AakrishtSP <akrisht62@gmail.com>
2024-08-31 01:05:40 +0545

    feature: added broad phase collision detection

--- src/Engine/CollisionManager.cpp
+++ src/Engine/CollisionManager.cpp
@@ -1,24 +1,24 @@
 #include "CollisionManager.hpp"
 
-//To check if a point passes origin with respect to a point
-bool CollisionManager::pointPassedOrigin (Vector2 refrenceVec, Vector2 testingVec){
+// To check if a point passes origin with respect to a point
+bool CollisionManager::pointPassedOrigin(Vector2 refrenceVec, Vector2 testingVec) {
     if (DotProduct(refrenceVec, testingVec) < 0)
         return false;
     else
         return true;
 }
 
-//To check if a point passes origin with respect to two points
-bool CollisionManager::pointPassedOrigin (Vector2 refrenceVec1, Vector2 refrenceVec2, Vector2 testingVec){
+// To check if a point passes origin with respect to two points
+bool CollisionManager::pointPassedOrigin(Vector2 refrenceVec1, Vector2 refrenceVec2, Vector2 testingVec) {
     Vector2 perpendiVec = directionToOrigin(refrenceVec1, refrenceVec2);
     if (DotProduct(perpendiVec, testingVec) < 0)
         return false;
-    else   
+    else
         return true;
 }
 
-//Gives perpendicular vector pointing towards orign 
-Vector2 CollisionManager::directionToOrigin (Vector2 vec1, Vector2 vec2){
+// Gives perpendicular vector pointing towards orign
+Vector2 CollisionManager::directionToOrigin(Vector2 vec1, Vector2 vec2) {
     Vector2 lineVec = vec2 - vec1;
     Vector2 perpendiVec = {lineVec.y, lineVec.x * -1};
     if (DotProduct(vec1, perpendiVec) < 0)
@@ -27,12 +27,12 @@ Vector2 CollisionManager::directionToOrigin (Vector2 vec1, Vector2 vec2){
     return Normalize(perpendiVec);
 }
 
-//Distance of a non-positional vector to origin
-float CollisionManager::distanceToOrigin (Vector2 vec1, Vector2 vec2){
+// Distance of a non-positional vector to origin
+float CollisionManager::distanceToOrigin(Vector2 vec1, Vector2 vec2) {
     return Magnitude(vec1) * cos(Angle(vec1, directionToOrigin(vec1, vec2) * -1));
 }
-//Returns Zero if the triangle contains the orign, else returns region in which the origin lies
-int CollisionManager::triangleContainOrigin (Vector2 oldVec1, Vector2 oldVec2, Vector2 recentVec){
+// Returns Zero if the triangle contains the orign, else returns region in which the origin lies
+int CollisionManager::triangleContainOrigin(Vector2 oldVec1, Vector2 oldVec2, Vector2 recentVec) {
     Vector2 vecRecnet = recentVec * -1;
     Vector2 perpendiVec = directionToOrigin(oldVec1, recentVec);
     if (DotProduct(vecRecnet, perpendiVec) > 0)
@@ -43,17 +43,15 @@ int CollisionManager::triangleContainOrigin (Vector2 oldVec1, Vector2 oldVec2, V
     return 0;
 }
 
-//Support function of Rectangle
-Vector2 CollisionManager::supportFunction(const Rect& rect, const Vector2& direction) {
+// Support function of Rectangle
+Vector2 CollisionManager::supportFunction(const Rect &rect, const Vector2 &direction) {
 
     Vector2 dir = Normalize(direction);
-    Vector2 vertices[4]= {
-        {rect.x, rect.y},
-        {rect.x + rect.width, rect.y},
-        {rect.x + rect.width, rect.y + rect.height},
-        {rect.x, rect.y + rect.height}
-    };
-    
+    Vector2 vertices[4] = {{rect.x, rect.y},
+                           {rect.x + rect.width, rect.y},
+                           {rect.x + rect.width, rect.y + rect.height},
+                           {rect.x, rect.y + rect.height}};
+
     Vector2 rotationPoint = {rect.x + rect.width / 2, rect.y + rect.height / 2};
     for (int i = 0; i < 4; i++) {
         RotatePoint(vertices[i], rotationPoint, rect.rotation);
@@ -72,43 +70,95 @@ Vector2 CollisionManager::supportFunction(const Rect& rect, const Vector2& direc
     return returnVertex;
 }
 
-//Support function of Circle
-Vector2 CollisionManager::supportFunction(const Circle &circle, const Vector2 &direction)
-{
+// Support function of Circle
+Vector2 CollisionManager::supportFunction(const Circle &circle, const Vector2 &direction) {
     Vector2 dir = Normalize(direction);
     return circle.center + dir * circle.radius;
 }
 
-//Simplex support function for two rectangles
-Vector2 CollisionManager::simplexSupportFunction(const Rect& rect1,const Rect& rect2 ,const Vector2& direction){
+// Simplex support function for two rectangles
+Vector2 CollisionManager::simplexSupportFunction(const Rect &rect1, const Rect &rect2, const Vector2 &direction) {
     Vector2 support1 = supportFunction(rect1, direction);
     Vector2 support2 = supportFunction(rect2, direction * -1);
     return support1 - support2;
 }
-//Simplex support function a rectangle and a circle
-Vector2 CollisionManager::simplexSupportFunction(const Rect& rect, const Circle& circle, const Vector2& direction){
+// Simplex support function a rectangle and a circle
+Vector2 CollisionManager::simplexSupportFunction(const Rect &rect, const Circle &circle, const Vector2 &direction) {
     Vector2 support1 = supportFunction(rect, direction);
     Vector2 support2 = supportFunction(circle, direction * -1);
     return support1 - support2;
 }
 
-//Simplex support function for two circles
-Vector2 CollisionManager::simplexSupportFunction(const Circle& circle1, const Circle& circle2, const Vector2& direction){
+// Simplex support function for two circles
+Vector2 CollisionManager::simplexSupportFunction(const Circle &circle1, const Circle &circle2,
+                                                 const Vector2 &direction) {
     Vector2 support1 = supportFunction(circle1, direction);
     Vector2 support2 = supportFunction(circle2, direction * -1);
     return support1 - support2;
 }
 
-void CollisionManager::updateAABBs(const std::vector<Collider> &colliders) {}
+void CollisionManager::update(float deltaTime) {
+    checkBroadCollisions();
+    checkNarrowCollisions();
+    resolveCollisions();
+    resetCollisions();
+}
 
-bool CollisionManager::checkAABBCollision(const AABB &aabb1, const AABB &aabb2) const { return false; }
+// void CollisionManager::addCollider(const Collider *collider) {
+//     const auto& colliderShapes = collider->getShapes();
 
-void CollisionManager::buildBVH(const std::vector<Collider> &colliders) {}
+//     // Add shapes if they are not already in the vector
+//     for (const auto& shape : colliderShapes) {
+//         if (std::find(shapes.begin(), shapes.end(), shape) == shapes.end()) {
+//             shapes.push_back(shape);
+//         }
+//     }
 
-bool CollisionManager::checkBVHCollision(const BVHNode &node1, const BVHNode &node2) const { return false; }
+//     // Add the collider if it's not already in the vector
+//     if (std::find(colliders.begin(), colliders.end(), collider) == colliders.end()) {
+//         colliders.push_back(std::shared_ptr<Collider>(collider));
+//     }
+// }
+
+void CollisionManager::resetColliders() {
+    colliders.clear();
+    shapes.clear();
+}
+
+void CollisionManager::checkBroadCollisions() {
+    // Reset the BVH tree
+    bvhRoot = std::make_unique<BVHNode>(shapes);
+    bvhRoot->subdivide(0, 10, 1);
+}
+
+void CollisionManager::checkNarrowCollisions() {}
+
+void CollisionManager::resolveCollisions() {}
+
+void CollisionManager::addPossibleCollision(std::vector<std::shared_ptr<CollisionShape>> colshapes) {
+    // Add the possible collision to the list
+    potentialCollisions.push_back(colshapes);
+
+    std::vector<std::shared_ptr<GameObject>> colShapesGO;
+    for (const auto& shape : colshapes) {
+        colShapesGO.push_back(std::shared_ptr<GameObject>(shape->getGameObject()));
+    }
+    potentialCollisionsGO.push_back(colShapesGO);
+}
+
+void CollisionManager::addActualCollision(const std::shared_ptr<CollisionShape> &shape1,
+                                          const std::shared_ptr<CollisionShape> &shape2) {}
+
+void CollisionManager::resetCollisions() {
+    actualCollisions.clear();
+    actualCollisionsGO.clear();
+    potentialCollisions.clear();
+    potentialCollisionsGO.clear();
+}
+
 
 // Check if two rectangles collide
-bool CollisionManager::didCollide (Rect rect1, Rect rect2){
+bool CollisionManager::didCollide(Rect rect1, Rect rect2) {
     Vector2 direction = Normalize(Vector2{rect2.x - rect1.x, rect2.y - rect1.y});
     Vector2 simplex1 = simplexSupportFunction(rect1, rect2, direction);
     direction = Normalize(simplex1 * -1);
@@ -120,8 +170,8 @@ bool CollisionManager::didCollide (Rect rect1, Rect rect2){
     Vector2 simplex3 = simplexSupportFunction(rect1, rect2, direction);
     Vector2 newSimplex;
     int region;
-    while (true){
-        if (!pointPassedOrigin(simplex1,simplex2,simplex3))
+    while (true) {
+        if (!pointPassedOrigin(simplex1, simplex2, simplex3))
             return false;
         region = triangleContainOrigin(simplex1, simplex2, simplex3);
         if (region == 0)
@@ -132,7 +182,7 @@ bool CollisionManager::didCollide (Rect rect1, Rect rect2){
             simplex1 = simplex3;
         direction = directionToOrigin(simplex1, simplex2);
         newSimplex = simplexSupportFunction(rect1, rect2, direction);
-        if(newSimplex == simplex1 || newSimplex == simplex2)
+        if (newSimplex == simplex1 || newSimplex == simplex2)
             return false;
         else
             simplex3 = newSimplex;
@@ -140,8 +190,8 @@ bool CollisionManager::didCollide (Rect rect1, Rect rect2){
     return false;
 }
 
-//Check if a rectangle and a circle collide
-bool CollisionManager::didCollide (Rect rect, Circle Cir){
+// Check if a rectangle and a circle collide
+bool CollisionManager::didCollide(Rect rect, Circle Cir) {
     Vector2 direction = Normalize(Vector2{Cir.center.x - rect.x, Cir.center.y - rect.y});
     Vector2 simplex1 = simplexSupportFunction(rect, Cir, direction);
     direction = Normalize(simplex1 * -1);
@@ -152,8 +202,8 @@ bool CollisionManager::didCollide (Rect rect, Circle Cir){
     Vector2 simplex3 = simplexSupportFunction(rect, Cir, direction);
     Vector2 newSimplex;
     int region;
-    while (true){
-        if (!pointPassedOrigin(simplex1,simplex2,simplex3))
+    while (true) {
+        if (!pointPassedOrigin(simplex1, simplex2, simplex3))
             return false;
         region = triangleContainOrigin(simplex1, simplex2, simplex3);
         if (region == 0)
@@ -164,7 +214,7 @@ bool CollisionManager::didCollide (Rect rect, Circle Cir){
             simplex1 = simplex3;
         direction = directionToOrigin(simplex1, simplex2);
         newSimplex = simplexSupportFunction(rect, Cir, direction);
-        if(newSimplex == simplex1 || newSimplex == simplex2)
+        if (newSimplex == simplex1 || newSimplex == simplex2)
             return false;
         else
             simplex3 = newSimplex;
@@ -172,8 +222,8 @@ bool CollisionManager::didCollide (Rect rect, Circle Cir){
     return false;
 }
 
-//Check if two circles collide
-bool CollisionManager::didCollide (Circle Cir1, Circle Cir2){
+// Check if two circles collide
+bool CollisionManager::didCollide(Circle Cir1, Circle Cir2) {
     float distance = Magnitude(Cir1.center - Cir2.center);
     if (distance < Cir1.radius + Cir2.radius)
         return true;
@@ -181,18 +231,18 @@ bool CollisionManager::didCollide (Circle Cir1, Circle Cir2){
         return false;
 }
 
-//For closest edge to origin
-Vector2 CollisionManager::closestEdgetoOrigin(){
+// For closest edge to origin
+Vector2 CollisionManager::closestEdgetoOrigin() {
     int n = polytope.size();
-    Vector2 closestEdge = {0, n-1};
-    float minDistance = distanceToOrigin(polytope.at(0), polytope.at(n-1));
+    Vector2 closestEdge = {0, (float) n - 1};
+    float minDistance = distanceToOrigin(polytope.at(0), polytope.at(n - 1));
     float distance;
     float point1, point2;
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         point1 = i;
-        point2 = (i+1)%n;
+        point2 = (i + 1) % n;
         distance = distanceToOrigin(polytope.at(point1), polytope.at(point2));
-        if (distance < minDistance){
+        if (distance < minDistance) {
             minDistance = distance;
             closestEdge = {point1, point2};
         }
@@ -200,26 +250,134 @@ Vector2 CollisionManager::closestEdgetoOrigin(){
     return closestEdge;
 }
 
-//For penetration vector of two rectangles
+// For penetration vector of two rectangles
 template<typename T>
-Vector2 CollisionManager::penetrationVector(T shp1, T shp2){
-    if(!didCollide(shp1,shp2))
-        return Vector2{0,0};
+Vector2 CollisionManager::penetrationVector(T shp1, T shp2) {
+    if (!didCollide(shp1, shp2))
+        return Vector2{0, 0};
     Vector2 closestEdge, newPolytope;
-    while(true){
+    while (true) {
         closestEdge = closestEdgetoOrigin();
-        newPolytope = simplexSupportFunction(shp1, shp2, directionToOrigin(polytope.at(closestEdge.x), polytope.at(closestEdge.y)));
-        if (newPolytope == polytope.at(closestEdge.x) || newPolytope == polytope.at(closestEdge.y)){
+        newPolytope = simplexSupportFunction(shp1, shp2,
+                                             directionToOrigin(polytope.at(closestEdge.x), polytope.at(closestEdge.y)));
+        if (newPolytope == polytope.at(closestEdge.x) || newPolytope == polytope.at(closestEdge.y)) {
             return directionToOrigin(polytope.at(closestEdge.x), polytope.at(closestEdge.y));
-        }
-        else
+        } else
             polytope.insert(polytope.begin() + closestEdge.y, newPolytope);
     }
+}
 
+AABB::AABB(const CollisionShape &shape) {
+    Rectangle bbox = shape.getBoundingBox();
+    float halfWidth = bbox.width / 2;
+    float halfHeight = bbox.height / 2;
+    min = {bbox.x - halfWidth, bbox.y - halfHeight};
+    max = {bbox.x + halfWidth, bbox.y + halfHeight};
+}
+
+AABB::AABB(const Circle &circle) {
+    min = circle.center - Vector2{circle.radius, circle.radius};
+    max = circle.center + Vector2{circle.radius, circle.radius};
+}
+
+AABB::AABB(const Rect &rect) {
+    float sinRot = sinf(rect.rotation);
+    float cosRot = cosf(rect.rotation);
+    Vector2 halfSize = {rect.width / 2, rect.height / 2};
+    Vector2 rotatedHalfSize = {fabs(halfSize.x * cosRot - halfSize.y * sinRot),
+                               fabs(halfSize.x * sinRot + halfSize.y * cosRot)};
+    min = Vector2{rect.x, rect.y} - rotatedHalfSize;
+    max = Vector2{rect.x, rect.y} + rotatedHalfSize;
+}
+
+AABB::AABB(const Rectangle &rect) {
+    Vector2 halfSize = {rect.width / 2, rect.height / 2};
+    min = Vector2{rect.x, rect.y} - halfSize;
+    max = Vector2{rect.x, rect.y} + halfSize;
 }
 
 // AABB collision check
-bool AABB::intersects(const AABB& other) const {
-    return (max.x >= other.min.x && min.x <= other.max.x &&
-            max.y >= other.min.y && min.y <= other.max.y);
+bool AABB::intersects(const AABB &other) const {
+    return (max.x >= other.min.x && min.x <= other.max.x && max.y >= other.min.y && min.y <= other.max.y);
+}
+
+AABB AABB::merge(const AABB &other) {
+    this->min = Vector2{std::min(this->min.x, other.min.x), std::min(this->min.y, other.min.y)};
+    this->max = Vector2{std::max(this->max.x, other.max.x), std::max(this->max.y, other.max.y)};
+    return *this;
+}
+
+
+AABB BVHNode::calculateBoundingBox() {
+    boundingBox = colliders.front()->getBoundingBox();
+    for (const auto &collider: colliders) {
+        boundingBox.merge(collider->getBoundingBox());
+    }
+    return boundingBox;
+}
+
+void BVHNode::subdivide(int currentDepth, int maxDepth, int minColliders) {
+    calculateBoundingBox();
+
+    // Check vertical or horizontal split
+    bool splitVertically = boundingBox.max.x - boundingBox.min.x >= boundingBox.max.y - boundingBox.min.y;
+
+    // Sort colliders based on their center along the split axis
+    std::sort(colliders.begin(), colliders.end(),
+              [splitVertically](const std::shared_ptr<CollisionShape> &a, const std::shared_ptr<CollisionShape> &b) {
+                  const AABB &aBox = a->getBoundingBox();
+                  const AABB &bBox = b->getBoundingBox();
+                  float centerA = splitVertically ? (aBox.min.x + aBox.max.x) / 2.0f : (aBox.min.y + aBox.max.y) / 2.0f;
+                  float centerB = splitVertically ? (bBox.min.x + bBox.max.x) / 2.0f : (bBox.min.y + bBox.max.y) / 2.0f;
+                  return centerA < centerB;
+              });
+
+    // Find the median index
+    size_t medianIndex = colliders.size() / 2;
+    AABB medianBox = colliders[medianIndex]->getBoundingBox();
+    float medianPosition =
+            splitVertically ? (medianBox.min.x + medianBox.max.x) / 2.0f : (medianBox.min.y + medianBox.max.y) / 2.0f;
+
+
+    // Create two child AABBs for the left and right nodes based on the median position
+    AABB leftBox = boundingBox;
+    AABB rightBox = boundingBox;
+
+    if (splitVertically) {
+        leftBox.max.x = medianPosition;
+        rightBox.min.x = medianPosition;
+    } else {
+        leftBox.max.y = medianPosition;
+        rightBox.min.y = medianPosition;
+    }
+
+    // Create left and right nodes
+    left = std::make_unique<BVHNode>(leftBox);
+    right = std::make_unique<BVHNode>(rightBox);
+
+    // Distribute colliders to left and right nodes
+    for (const auto &collider: colliders) {
+        const AABB &colliderBox = collider->getBoundingBox();
+        if (colliderBox.intersects(leftBox)) {
+            left->colliders.push_back(collider);
+        }
+        if (colliderBox.intersects(rightBox)) {
+            right->colliders.push_back(collider);
+        }
+    }
+
+    // Clear colliders in the current node after distributing them
+    colliders.clear();
+
+    // Recursively subdivide the left and right nodes
+    if (left->colliders.size() > minColliders && currentDepth < maxDepth) {
+        left->subdivide(currentDepth + 1, maxDepth, minColliders);
+    } else {
+
+    }
+    if (right->colliders.size() > minColliders && currentDepth < maxDepth) {
+        right->subdivide(currentDepth + 1, maxDepth, minColliders);
+    } else {
+
+    }
 }
--- src/Engine/CollisionManager.hpp
+++ src/Engine/CollisionManager.hpp
@@ -1,77 +1,104 @@
 #pragma once
-#include "Vector2Ext.hpp"
 #include "Components/Collider.hpp"
+#include "Vector2Ext.hpp"
+#include <set>
 
 class AABB;
 class BVHNode;
-class CollisionManager{
-    public:
-    static CollisionManager& getInstance() {
+class CollisionManager {
+public:
+    static CollisionManager &getInstance() {
         static CollisionManager instance;
         return instance;
     }
 
-        // Broad phase collision detection
-    void updateAABBs(const std::vector<Collider>& colliders);
-    bool checkAABBCollision(const AABB& aabb1, const AABB& aabb2) const;
-    void buildBVH(const std::vector<Collider>& colliders);
-    bool checkBVHCollision(const BVHNode& node1, const BVHNode& node2) const;
+    bool didCollide(Rect rect1, Rect rect2);
+    bool didCollide(Circle Cir1, Circle Cir2);
+    bool didCollide(Rect rect, Circle Cir);
+    bool didCollide(Circle Cir, Rect rect) { return didCollide(rect, Cir); };
 
-
-    bool didCollide (Rect rect1, Rect rect2);
-    bool didCollide (Circle Cir1, Circle Cir2);
-    bool didCollide (Rect rect, Circle Cir);
-    bool didCollide (Circle Cir, Rect rect){return didCollide(rect, Cir);};
-
-    //For EPA
+    // For EPA
     template<typename T>
     Vector2 penetrationVector(T rect1, T rect2);
     Vector2 closestEdgetoOrigin();
 
-    int triangleContainOrigin (Vector2 vec1, Vector2 vec2, Vector2 vec3);
-    bool pointPassedOrigin (Vector2 refrenceVec, Vector2 testingVec);
-    bool pointPassedOrigin (Vector2 refrenceVec1, Vector2 refrenceVec2, Vector2 testingVec);
-    float distanceToOrigin (Vector2 vec1, Vector2 vec2);
-    Vector2 directionToOrigin (Vector2 vec1, Vector2 vec2);
-    Vector2 supportFunction(const Rect& rect, const Vector2& direction);
+    int triangleContainOrigin(Vector2 vec1, Vector2 vec2, Vector2 vec3);
+    bool pointPassedOrigin(Vector2 refrenceVec, Vector2 testingVec);
+    bool pointPassedOrigin(Vector2 refrenceVec1, Vector2 refrenceVec2, Vector2 testingVec);
+    float distanceToOrigin(Vector2 vec1, Vector2 vec2);
+    Vector2 directionToOrigin(Vector2 vec1, Vector2 vec2);
+    Vector2 supportFunction(const Rect &rect, const Vector2 &direction);
     Vector2 supportFunction(const Circle &circle, const Vector2 &direction);
-    Vector2 simplexSupportFunction(const Rect& rect1,const Rect& rect2 ,const Vector2& direction);
-    Vector2 simplexSupportFunction(const Rect& rect, const Circle& circle, const Vector2& direction);
-    Vector2 simplexSupportFunction(const Circle& circle1, const Circle& circle2, const Vector2& direction);
+    Vector2 simplexSupportFunction(const Rect &rect1, const Rect &rect2, const Vector2 &direction);
+    Vector2 simplexSupportFunction(const Rect &rect, const Circle &circle, const Vector2 &direction);
+    Vector2 simplexSupportFunction(const Circle &circle1, const Circle &circle2, const Vector2 &direction);
+
+    void update(float deltaTime);
+    void renderUpdate(float renderDeltaTime);
+
+    // void addCollider(const Collider *collider);
+    void resetColliders();
+
+    void checkBroadCollisions();
+    void checkNarrowCollisions();
+
+    void resolveCollisions();
+
+    void addPossibleCollision(std::vector<std::shared_ptr<CollisionShape>>);
+    void addActualCollision(const std::shared_ptr<CollisionShape> &shape1,
+                            const std::shared_ptr<CollisionShape> &shape2);
+    void resetCollisions();
 
 
+private:
+    CollisionManager() = default;
+    CollisionManager(const CollisionManager &) = delete;
+    CollisionManager &operator=(const CollisionManager &) = delete;
+
+    std::unique_ptr<BVHNode> bvhRoot; // Root of the BVH tree
+    std::vector<std::shared_ptr<Collider>> colliders; // List of colliders
+    std::vector<std::shared_ptr<CollisionShape>> shapes; // List of collision shapes
+    std::vector<std::vector<std::shared_ptr<CollisionShape>>>
+            potentialCollisions; // List of colliders that are potentially colliding
+    std::vector<std::vector<std::shared_ptr<CollisionShape>>>
+            actualCollisions; // List of colliders that are actually colliding
+    std::vector<std::vector<std::shared_ptr<GameObject>>>
+            potentialCollisionsGO; // List of GameObjects that are potentially colliding
+    std::vector<std::vector<std::shared_ptr<GameObject>>>
+            actualCollisionsGO; // List of GameObjects that are actually colliding
 
 
-    private:
-        CollisionManager() = default;
-        CollisionManager(const CollisionManager&) = delete;
-        CollisionManager& operator=(const CollisionManager&) = delete;
-        //std::unordered_map<std::string, vector<>> creators;
-        std::vector<AABB> aabbs;  // List of AABBs for broad-phase collision detection
-        std::unique_ptr<BVHNode> bvhRoot;  // Root of the BVH tree
-        std::vector<Vector2> polytope;
+    std::vector<Vector2> polytope;
 };
 
 // AABB structure definition
 class AABB {
+public:
     Vector2 min;
     Vector2 max;
-
-public:
-    AABB() = default;
-    AABB(const Vector2& min, const Vector2& max) : min(min), max(max) {}
-
-    bool intersects(const AABB& other) const;
+    AABB() : min(Vector2({0, 0})), max(Vector2({0, 0})) {};
+    AABB(const Vector2 &min, const Vector2 &max) : min(min), max(max) {}
+    AABB(const CollisionShape &shape);
+    AABB(const Circle &circle);
+    AABB(const Rect &rect);
+    AABB(const Rectangle &rect);
+    bool intersects(const AABB &other) const;
+    AABB merge(const AABB &other);
 };
 
 // BVH Node structure definition
 class BVHNode {
 public:
     AABB boundingBox;
-    std::vector<std::shared_ptr<Collider>> colliders;  // Colliders within this node
+    std::vector<std::shared_ptr<CollisionShape>> colliders; // Colliders within this node
     std::unique_ptr<BVHNode> left;
     std::unique_ptr<BVHNode> right;
 
     BVHNode() = default;
-    BVHNode(const AABB& box) : boundingBox(box) {}
+    BVHNode(const AABB &box) : boundingBox(box) {}
+    BVHNode(const std::vector<std::shared_ptr<CollisionShape>> &colliders) : colliders(colliders) {}
+
+    AABB calculateBoundingBox();
+
+    void subdivide(int currentDepth, int maxDepth = 10, int minColliders = 1);
 };
