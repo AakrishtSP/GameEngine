e0ab10f9672e7a6ce3a37375e545eea830cb38c6
Ishan Gautam <isangautam@gmail.com>
2024-11-02 20:30:53 +0545

    ...

--- build/_deps/raygui-subbuild/CMakeFiles/Makefile.cmake
+++ build/_deps/raygui-subbuild/CMakeFiles/Makefile.cmake
@@ -10,10 +10,8 @@ set(CMAKE_MAKEFILE_DEPENDS
   "CMakeFiles/3.30.3/CMakeSystem.cmake"
   "CMakeLists.txt"
   "raygui-populate-prefix/tmp/raygui-populate-mkdirs.cmake"
-  "/usr/share/cmake/Modules/CMakeDetermineSystem.cmake"
   "/usr/share/cmake/Modules/CMakeGenericSystem.cmake"
   "/usr/share/cmake/Modules/CMakeInitializeConfigs.cmake"
-  "/usr/share/cmake/Modules/CMakeSystem.cmake.in"
   "/usr/share/cmake/Modules/CMakeSystemSpecificInformation.cmake"
   "/usr/share/cmake/Modules/CMakeSystemSpecificInitialize.cmake"
   "/usr/share/cmake/Modules/ExternalProject.cmake"
@@ -38,7 +36,6 @@ set(CMAKE_MAKEFILE_OUTPUTS
 
 # Byproducts of CMake generate step:
 set(CMAKE_MAKEFILE_PRODUCTS
-  "CMakeFiles/3.30.3/CMakeSystem.cmake"
   "raygui-populate-prefix/tmp/raygui-populate-mkdirs.cmake"
   "raygui-populate-prefix/src/raygui-populate-stamp/download-raygui-populate.cmake"
   "raygui-populate-prefix/src/raygui-populate-stamp/extract-raygui-populate.cmake"
--- build/_deps/raylib-subbuild/CMakeFiles/Makefile.cmake
+++ build/_deps/raylib-subbuild/CMakeFiles/Makefile.cmake
@@ -10,10 +10,8 @@ set(CMAKE_MAKEFILE_DEPENDS
   "CMakeFiles/3.30.3/CMakeSystem.cmake"
   "CMakeLists.txt"
   "raylib-populate-prefix/tmp/raylib-populate-mkdirs.cmake"
-  "/usr/share/cmake/Modules/CMakeDetermineSystem.cmake"
   "/usr/share/cmake/Modules/CMakeGenericSystem.cmake"
   "/usr/share/cmake/Modules/CMakeInitializeConfigs.cmake"
-  "/usr/share/cmake/Modules/CMakeSystem.cmake.in"
   "/usr/share/cmake/Modules/CMakeSystemSpecificInformation.cmake"
   "/usr/share/cmake/Modules/CMakeSystemSpecificInitialize.cmake"
   "/usr/share/cmake/Modules/ExternalProject.cmake"
@@ -38,7 +36,6 @@ set(CMAKE_MAKEFILE_OUTPUTS
 
 # Byproducts of CMake generate step:
 set(CMAKE_MAKEFILE_PRODUCTS
-  "CMakeFiles/3.30.3/CMakeSystem.cmake"
   "raylib-populate-prefix/tmp/raylib-populate-mkdirs.cmake"
   "raylib-populate-prefix/src/raylib-populate-stamp/download-raylib-populate.cmake"
   "raylib-populate-prefix/src/raylib-populate-stamp/extract-raylib-populate.cmake"
